a778c926e388c5b1a906f2cdec4bb038960553d7
Lucas Vienna <dev@lucasvienna.dev>
2021-12-18 09:00:18 +0100

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  rules: {
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-console': ['error', { allow: ['debug', 'warn', 'error'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'no-use-before-define': 'off', // note you must disable the base rule as it can report incorrect errors
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+  },
+};
