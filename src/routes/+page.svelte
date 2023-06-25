ed950e153e21d6edbb0a53e252d7cc0e2feab4bf
Lucas Vienna <dev@lucasvienna.dev>
2023-06-25 23:30:23 +0200

    refactor: improve the landing page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,22 +18,53 @@
 <Heading title="Lucas Vienna" subtitle="Software Engineer | Product Manager" />
 
 <section role="main">
-	My resumé, portfolio, and development blog. This is where I talk about code, tech, my projects,
-	and anything related to my professional life. You'll see posts about tools, languages, frameworks,
-	apps, websites and more. Mostly I'll talk about my current project, the tooling I'm using and
-	how/why I'm doing things this way or that.
+	<p>
+		I'm a software engineer and product manager with a passion for creating innovative solutions and
+		exploring the latest technologies. Whether it's diving into the world of containers, automating
+		repetitive tasks, leveraging the power of Rust language, or building dynamic web applications
+		with Svelte and SvelteKit, I'm constantly seeking new avenues to expand my skills.
+	</p>
+
+	<p>
+		On this site, you'll find a collection of blog posts where I share insights into my current
+		projects, my programming methodologies, and intriguing pieces of software that have caught my
+		attention. I believe in the power of knowledge sharing and aim to provide valuable resources for
+		fellow developers, enthusiasts, and anyone curious about the tech world.
+	</p>
+
+	<p>
+		Additionally, my portfolio section showcases a range of personal projects I've worked on. These
+		projects reflect my dedication to innovation, problem-solving, and craftsmanship in software
+		development. Feel free to explore and get a glimpse into the projects that have shaped my
+		journey.
+	</p>
+
+	<p>
+		If you're interested in collaborating on exciting ventures or discussing opportunities, I'm
+		always open to new connections. Don't hesitate to reach out!
+	</p>
+
+	<p>Thank you for visiting, and I hope you find inspiration and insights within these pages.</p>
 </section>
 
 <section role="feed">
+	<h3>Recent Posts</h3>
+
 	<PostsList {posts} />
 
-	<div>
-		<a href="{base}/blog">See all posts</a>
-	</div>
+	{#if data.posts.length > 3}
+		<div>
+			<a href="{base}/blog">See all posts</a>
+		</div>
+	{/if}
 </section>
 
 <style>
 	section[role='feed'] div:last-of-type {
 		text-align: center;
+	}
+
+	h3 {
+		margin-bottom: 1rem;
 	}
 </style>
