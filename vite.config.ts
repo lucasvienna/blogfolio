da59c0aa748c395d9f1eee02b6821feffa9cc082
Lucas Vienna <dev@lucasvienna.dev>
2025-11-08 09:09:36 +0100

    test: let vitest handle github reporting automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { defineConfig } from "vitest/config";
-import GithubActionsReporter from "vitest-github-actions-reporter";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		reporters: process.env.GITHUB_ACTIONS ? ["default", new GithubActionsReporter()] : "default",
 		coverage: {
 			include: ["src/**/*.{ts,svelte}"]
 		},
