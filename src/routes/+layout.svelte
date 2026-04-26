bd6438c58f0dbdc72c0bac4802d6952e6236dd46
Lucas Vienna <dev@lucasvienna.dev>
2026-04-26 09:31:12 +0200

    fix(deps): upgrade dependencies, switch to direct pico import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import "@picocss/pico";
+	import "@picocss/pico/css/pico.min.css";
 	import Footer from "./Footer.svelte";
 	import Header from "./Header.svelte";
 	import "./styles.css";
